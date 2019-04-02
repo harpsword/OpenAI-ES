f128276a7e7cbaa6ff89d601b509e4c8b9835050
harpsword <harpswordyyl@gmail.com>
2019-04-02 13:29:53 +0800

    finish es-rl

--- es-rl-try.py
+++ es-rl-try.py
@@ -34,9 +34,10 @@ N_POPULATION = 2*N_KID
 N_GENERATION = 1000         # training step
 LR = 0.05
 SIGMA = 0.05
-N_ACTION = 10
+N_ACTION = 7
 # N_CORE = mp.cpu_count() * 2 - 8
-N_CORE = mp.cpu_count() - 1
+# N_CORE = mp.cpu_count() - 1
+N_CORE = 2
 
 class SGD(object): 
 
@@ -49,14 +50,14 @@ class SGD(object):
             # print(params.data.size())
         
     def update_model_parameters(self, model, gradients):
-        if not isinstance(gradients, Dict):
+        if not isinstance(gradients, dict):
             raise TypeError("the gradients must be a dict with key(name)"
                             "value(torch.tensor), Got{}".format(torch.typename(gradients)))
         for name, params in self.v.items():
             self.v[name].mul_(self.momentum)
             self.v[name].add_(gradients[name]*(1-self.momentum))
         # update parameter of model recursively
-        with torch.no_grad:
+        with torch.no_grad():
             for name, param in model.named_parameters():
                 '''
                 example : name(conv1.weight)
@@ -81,18 +82,18 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(50, N_ACTION)
 
     def forward(self, x):
-        print(x.shape)
+        # print(x.shape)
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        print(x.shape)
+        # print(x.shape)
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        print(x.shape)
+        # print(x.shape)
         x = F.relu(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2))
-        print(x.shape)
+        # print(x.shape)
         x = x.view(-1, 600*20)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return F.softmax(x, dim=1)
 
 def sign(k_id): return 1. if k_id % 2 == 0 else -1.  # mirrored sampling
 
@@ -111,7 +112,8 @@ def get_reward(model, env, ep_max_step, seed_and_id=None):
     for step in range(ep_max_step):
         # print(trans(observation).size())
         # print(type(observation))
-        action = model(trans(observation).reshape(1, 3, 250, 160))
+        action = model(trans(observation).reshape(1, 3, 250, 160)).argmax().item()
+        # print(action)
         observation, reward , done, _ = env.step(action)
         ep_r += reward
         if done:
@@ -123,11 +125,12 @@ def train(model, optimizer, utility, pool):
     # mirrored sampling
     noise_seed = np.random.randint(0, 2 ** 32 - 1, size=N_KID, dtype=np.uint32).repeat(2)   
 
-    # # distribute training in parallel
-    # jobs = [pool.apply_async(get_reward, ( model, env, CONFIG['ep_max_step'],
-    #                                       [noise_seed[k_id], k_id], )) for k_id in range(N_KID*2)]
-    # rewards = np.array([j.get() for j in jobs])
-    rewards = [get_reward(model, env, CONFIG['ep_max_step'], None,)]
+    # distribute training in parallel
+    jobs = [pool.apply_async(get_reward, ( model, env, CONFIG['ep_max_step'],
+                                          [noise_seed[k_id], k_id], )) for k_id in range(N_KID*2)]
+    rewards = np.array([j.get() for j in jobs])
+    print(rewards)
+    # rewards = [get_reward(model, env, CONFIG['ep_max_step'], None,)]
     kids_rank = np.argsort(rewards)[::-1]               # rank kid id by reward
 
     cumulative_update = {}       # initialize update values
@@ -137,8 +140,10 @@ def train(model, optimizer, utility, pool):
         for name, params in model.named_parameters():
             if not name in cumulative_update.keys():
                 cumulative_update[name] = torch.zeros_like(params, dtype=torch.float)
-            cumulative_update[name] += utility[ui] * sign(k_id) * torch.randn_like(params)
-    optimizer.update_model_parameters(model, cumulative_update/(2*N_KID*SIGMA))
+            cumulative_update[name].add_(utility[ui] * sign(k_id) * torch.randn_like(params))
+    for name, params in cumulative_update.items():
+        cumulative_update[name].mul_(1/(2*N_KID*SIGMA))
+    optimizer.update_model_parameters(model, cumulative_update)
     return model, rewards
 
 if __name__ == '__main__':
@@ -161,10 +166,11 @@ if __name__ == '__main__':
         t0 = time.time()
         model, kid_rewards = train(model, optimizer, utility, pool)
 
-        if g % 20 == 0:
+        # if g % 20 == 0:
+        if True:
             mar = 0
             for j in range(5):
-                # test trained net without noise
+                # test trained net without noises
                 net_r = get_reward(model, env, CONFIG['ep_max_step'], None,)
                 # mar = net_r if mar is None else 0.9 * mar + 0.1 * net_r       # moving average reward
                 mar += net_r
@@ -172,7 +178,7 @@ if __name__ == '__main__':
             print(
                 'Gen: ', g,
                 '| Net_R: %.1f' % mar,
-                '| Kid_avg_R: %.1f' % kid_rewards.mean(),
+                '| Kid_avg_R: %.1f' % np.array(kid_rewards).mean(),
                 '| Gen_T: %.2f' % (time.time() - t0),)
         if mar >= CONFIG['eval_threshold']: break
     
